0e5a44da49c500f0e40573ce094e2326663bdd3f
lorenzoash <z.lakyo@gmail.com>
2018-06-30 15:22:31 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Title from "./Components/Title"
+import "./App.css"
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Title />
+      </div>
+    );
+  }
+}; 
+
+export default App;
